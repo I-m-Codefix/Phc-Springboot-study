25f0bcdb72fa15c09ce5eaa3d48aac4fea8d3148
hc Park <hyunchun24@gmail.com>
2022-04-04 20:25:34 +0900

    commit

---
+++ phc-spring/phc-spring/src/main/java/imcodefix/phcspring/PhcSpringApplication.java
@@ -0,0 +1,13 @@
+package imcodefix.phcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhcSpringApplication.class, args);
+	}
+
+}
---
+++ phc-spring/phc-spring/src/test/java/imcodefix/phcspring/PhcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package imcodefix.phcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
